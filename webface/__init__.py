cf2759951e19c4aa3dba1ce2d644c45e07e6b342
LGib <gib36099@spseol.cz>
2019-12-09 10:30:48 +0100

    templates a static

--- webface/__init__.py
+++ webface/__init__.py
@@ -2,3 +2,5 @@ import flask
 
 app = flask.Flask(__name__)
 app.secret_key = b'Ka78584554lorst Josef os.urandom(24)'
+
+from . import routes
--- webface/routes.py
+++ webface/routes.py
@@ -1,3 +1,4 @@
+from . import app
 
 @app.route('/')
 def index():
