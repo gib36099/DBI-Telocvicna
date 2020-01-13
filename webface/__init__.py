70dd3cdc6e98f504213477b3af26eee5559bee88
LGib <gib36099@spseol.cz>
2020-01-13 10:07:39 +0100

    uzivatel

--- webface/__init__.py
+++ webface/__init__.py
@@ -1,6 +1,9 @@
 import flask
+from flask_misaka import Misaka
 
 app = flask.Flask(__name__)
+Misaka(app)
 app.secret_key = b'Ka78584554lorst Josef os.urandom(24)'
 
 from . import routes
+from . import models
